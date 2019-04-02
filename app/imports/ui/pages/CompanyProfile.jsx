a8b208f889c267092c83b952c202c6e2726ecacd
cardinalli-uh <rcard@hawaii.edu>
2019-04-01 14:36:23 -1000

    Create CompanyProfile.jsx

---
+++ app/imports/ui/pages/CompanyProfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import {  } from 'semantic-ui-react';
+
+
+/** A simple static component to render some text for the landing page. */
+class CompanyProfile extends React.Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default CompanyProfile;
