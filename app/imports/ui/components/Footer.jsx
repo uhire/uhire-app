b150bb6a63c3ec1ba8c8801664fc2f50c98909ce
cardinalli-uh <rcard@hawaii.edu>
2019-04-01 14:09:22 -1000

    Update Footer.jsx
    Footer is implemented as a menu, items can be added.

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
     return (
         <Menu borderless fixed='bottom'>
-          <Menu.Item>About</Menu.Item>
-          <Menu.Item>Let me know what else...</Menu.Item>
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/about" key='about'>About</Menu.Item>
+          <Menu.Item>Let me know what else... or how else...</Menu.Item>
         </Menu>
     );
   }
