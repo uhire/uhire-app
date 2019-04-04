338bd9add0bf2ffef863fb9a9994c9e3721ae98c
Keoni Fontanilla <keoni.fontanilla@gmail.com>
2019-04-04 11:34:32 -1000

    added CompanySchema

---
+++ app/imports/api/company/company.js
@@ -0,0 +1,23 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Create a Meteor collection. */
+const Companies = new Mongo.Collection('Companies');
+
+/** Create a schema to constrain the structure of documents associated with this collection. */
+const CompanySchema = new SimpleSchema({
+  companyName: String,
+  location: String,
+  description: String,
+  image: String,
+  positions: String,
+  contact: String,
+  owner: String,
+}, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+Companies.attachSchema(CompanySchema);
+
+/** Make the collection and schema available to other code. */
+export { Companies, CompanySchema };
