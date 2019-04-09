cedb770676bd38d08896fc22bed4cecb6e355975
Keoni Fontanilla <keoni.fontanilla@gmail.com>
2019-04-08 16:48:43 -1000

    Update company.js

--- app/imports/api/company/company.js
+++ app/imports/api/company/company.js
@@ -5,7 +5,7 @@ import { Tracker } from 'meteor/tracker';
 /** Create a Meteor collection. */
 const Companies = new Mongo.Collection('Companies');
 
-/** Create a schema to constrain the structure of documents associated with this collection. */
+/** Create a schema to constrain the structure of documents associated with this collection */
 const CompanySchema = new SimpleSchema({
   companyName: String,
   location: String,
