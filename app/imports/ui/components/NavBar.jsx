b3bf6f161d971e03b25f20a5b7a37781a481d9db
Peter Newton <46580447+peter-newton@users.noreply.github.com>
2019-04-08 19:38:32 -1000

    NavBar Position edit

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -11,38 +11,38 @@ class NavBar extends React.Component {
   render() {
     const menuStyle = { marginBottom: '10px' };
     return (
-          <Menu style={menuStyle} attached="top" borderless inverted color='grey'>
-            <Menu.Item as={NavLink} activeClassName="" exact to="/">
-              <Header inverted as='h1'>UHire</Header>
-            </Menu.Item>
-            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-                <Dropdown item text='Categories'>
+        <Menu style={menuStyle} attached="top" borderless inverted color='grey'>
+          <Menu.Item as={NavLink} activeClassName="" exact to="/">
+            <Header inverted as='h1'>UHire</Header>
+          </Menu.Item>
+          {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+              <Dropdown item text='Categories'>
+                <Dropdown.Menu>
+                  <Dropdown.Item icon="user" text="Admin Home Page" as={NavLink} exact to="/admin"/>
+                  <Dropdown.Item icon="briefcase" text="Add Company Page" as={NavLink} exact to="/add"/>
+                  <Dropdown.Item icon="briefcase" text="Company Profile Page" as={NavLink} exact to="/list"/>
+                  <Dropdown.Item icon="briefcase" text="Company Home Page" as={NavLink} exact to="/cohome"/>
+                  <Dropdown.Item icon="student" text="Student Profile Page" as={NavLink} exact to="/admin"/>
+                  <Dropdown.Item icon="student" text="Student Home Page" as={NavLink} exact to="/studentHome"/>
+                </Dropdown.Menu>
+              </Dropdown>) : ''}
+          <Menu.Item position="right">
+            {this.props.currentUser === '' ? (
+                <Dropdown text="Login" pointing="top right" icon={'user'}>
                   <Dropdown.Menu>
-                    <Dropdown.Item icon="user" text="Admin Home Page" as={NavLink} exact to="/admin"/>
-                    <Dropdown.Item icon="briefcase" text="Company Home Page" as={NavLink} exact to="/cohome"/>
-                    <Dropdown.Item icon="briefcase" text="Company Profile Page" as={NavLink} exact to="/list"/>
-                    <Dropdown.Item icon="briefcase" text="Add Company Page" as={NavLink} exact to="/add"/>
-                    <Dropdown.Item icon="student" text="Student Home Page" as={NavLink} exact to="/studentHome"/>
-                    <Dropdown.Item icon="student" text="Student Profile Page" as={NavLink} exact to="/admin"/>
+                    <Dropdown.Item icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
+                    <Dropdown.Item icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
                   </Dropdown.Menu>
-                </Dropdown>) : ''}
-            <Menu.Item position="right">
-              {this.props.currentUser === '' ? (
-                  <Dropdown text="Login" pointing="top right" icon={'user'}>
-                    <Dropdown.Menu>
-                      <Dropdown.Item icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
-                      <Dropdown.Item icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
-                    </Dropdown.Menu>
-                  </Dropdown>
-              ) : (
-                  <Dropdown text={this.props.currentUser} pointing="top right" icon={'user'}>
-                    <Dropdown.Menu>
-                      <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
-                    </Dropdown.Menu>
-                  </Dropdown>
-              )}
-            </Menu.Item>
-          </Menu>
+                </Dropdown>
+            ) : (
+                <Dropdown text={this.props.currentUser} pointing="top right" icon={'user'}>
+                  <Dropdown.Menu>
+                    <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
+                  </Dropdown.Menu>
+                </Dropdown>
+            )}
+          </Menu.Item>
+        </Menu>
     );
   }
 }
