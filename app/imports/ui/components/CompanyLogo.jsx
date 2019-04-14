9b4aed5a6202cd5b636042be3f74873dcfa57613
Keoni Fontanilla <keoni.fontanilla@gmail.com>
2019-04-14 11:08:04 -1000

    able to edit individual fields

--- app/imports/ui/components/CompanyLogo.jsx
+++ app/imports/ui/components/CompanyLogo.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
-import { Image, Button } from 'semantic-ui-react';
+import { Image, Button, Modal, Segment } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
+import { Companies, CompanySchema } from '/imports/api/company/company';
+import { Bert } from 'meteor/themeteorchef:bert';
+import AutoForm from 'uniforms-semantic/AutoForm';
+import TextField from 'uniforms-semantic/TextField';
+import SubmitField from 'uniforms-semantic/SubmitField';
+import HiddenField from 'uniforms-semantic/HiddenField';
+import ErrorsField from 'uniforms-semantic/ErrorsField';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Company extends React.Component {
+
+  submit(data) {
+    const { companyName, location, description, contact, image, _id } = data;
+    Companies.update(_id, { $set: { companyName, location, description, contact, image } }, (error) => (error ?
+        Bert.alert({ type: 'danger', message: `Update failed: ${error.message}` }) :
+        Bert.alert({ type: 'success', message: 'Update succeeded' })));
+  }
+
   render() {
     return (
         <div>
           <Image src={this.props.company.image} size='medium' floated='right'/>
+          <Modal trigger={<Button size='mini'>Edit</Button>}>
+            <AutoForm schema={CompanySchema} onSubmit={this.submit} model={this.props.company}>
+              <Segment>
+                <TextField name='image'/>
+                <SubmitField value='Submit'/>
+                <ErrorsField/>
+                <HiddenField name='companyName' value={this.props.company.companyName}/>
+                <HiddenField name='location' value={this.props.company.location}/>
+                <HiddenField name='description' value={this.props.company.description}/>
+                <HiddenField name='contact' value={this.props.company.contact}/>
+                <HiddenField name='owner' value={this.props.company.owner}/>
+              </Segment>
+            </AutoForm>
+          </Modal>
           <Button>
             <Link to={'/add/'}>Add Company</Link>
           </Button>
@@ -19,6 +48,7 @@ class Company extends React.Component {
 
 /** Require a document to be passed to this component. */
 Company.propTypes = {
+  model: PropTypes.object,
   company: PropTypes.object.isRequired,
 };
 
