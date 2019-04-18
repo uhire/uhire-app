f0611ab4bfbe76e97162eb073c6b4e0069e7edda
Peter Newton <46580447+peter-newton@users.noreply.github.com>
2019-04-18 09:42:26 -1000

    Updated default call

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -28,12 +28,11 @@ Meteor.methods({
 });
 
 /** When running app for first time, pass a settings file to set up a default user account. */
-// if (Meteor.users.find().count() === 0) {
-//   if (Meteor.settings.defaultAccounts) {
-//     console.log('Creating the default user(s)');
-//     Meteor.settings.defaultAccounts.map(
-//     ({ email, password, role }) => Meteor.call('createUser', email, password, role));
-//   } else {
-//     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
-//   }
-// }
+if (Meteor.users.find().count() === 0) {
+  if (Meteor.settings.defaultAccounts) {
+    console.log('Creating the default user(s)');
+    Meteor.settings.defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
+  } else {
+    console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
+  }
+}
