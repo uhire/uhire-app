64dbb22151617415534fa748081702e116c65424
John Yap <46661979+JKYap808@users.noreply.github.com>
2019-04-30 17:42:45 -1000

    fix variables

--- app/imports/ui/pages/StudentHome.jsx
+++ app/imports/ui/pages/StudentHome.jsx
@@ -49,28 +49,31 @@ class StudentHome extends React.Component {
       return <Redirect to={'/addStudent'}/>;
     }
 
-    const test1 = function (string, array) {
+    let test1 = function(string, array){
       return _.contains(array, string);
     };
 
     const interests2 = this.props.students[0].interests;
 
-    const test2 = function (array1, array2) {
+    let test2 = function (array1, array2){
       let bool1 = false;
 
-      array1.forEach(function (element) {
+      array1.forEach(function(element){
 
         bool1 = bool1 || test1(element, array2);
-        if (bool1 === true) {
+        if (bool1 == true) {
+          console.log(bool1);
           return bool1;
         }
-         return bool1;
       });
+      console.log(bool1);
+      return bool1;
+
     };
 
     if (this.state.data == null) {
       this.state.data = this.props.positions;
-      const data2 = this.state.data.filter(position => test2(position.interests, interests2));
+      const data2 = this.state.data.filter( position => test2(position.interests, interests2));
       this.state.data = data2;
     }
 
