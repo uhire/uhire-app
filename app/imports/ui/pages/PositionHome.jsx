238a61d012040056542fd914553eb49439906c0c
John Yap <46661979+JKYap808@users.noreply.github.com>
2019-04-30 19:02:16 -1000

    Update PositionHome.jsx

--- app/imports/ui/pages/PositionHome.jsx
+++ app/imports/ui/pages/PositionHome.jsx
@@ -48,7 +48,7 @@ class PositionHome extends React.Component {
 
         <div className="page-filler">
           <Container>
-            <Header as="h2" textAlign="center" inverted>Student Profiles </Header>
+            <Header as="h2" textAlign="center" inverted>Profiles of applicants for the {this.props.doc.title} position</Header>
             <CardGroup centered>
 
               {this.state.students.map((stuff) =>
